3d3b0d20d739d6a4840143b4fa01b95f585f7d09
Kohei TAKATA <kt.koheitakata@gmail.com>
2017-05-22 23:46:55 +0900

    Add file system

--- App.js
+++ App.js
@@ -13,10 +13,14 @@ import {
     Drawer,
     Card,
     CardItem,
+    Fab,
 } from 'native-base';
 
 import SideBar from './SideBar.js';
 import NoteModal from './NoteModal';
+
+import RNFetchBlob from 'react-native-fetch-blob';
+const fs = RNFetchBlob.fs;
 
 export default class App extends Component {
     constructor() {
@@ -26,7 +30,38 @@ export default class App extends Component {
         };
     }
 
-    componentWillMount() {}
+    componentWillMount() {
+        const dirs = RNFetchBlob.fs.dirs;
+        this.listFiles(dirs.DocumentDir)
+            .then((files) => {
+                // Check whether the 'Boostnote' folder be or not.
+                const filteredFiles = files.filter((name) => {
+                    return name === 'Boostnote';
+                });
+                // If not, create.
+                if (filteredFiles.length === 0) {
+                    this.createDir();
+                }
+                return this.listFiles(`${dirs.DocumentDir}/Boostnote`);
+            })
+            .then((files) => {
+                // Check whether the folder has any files or not.
+
+                if (files.length === 0) {
+                    // If not, create.
+                    // TODO change to correct file name
+                    fs.createFile(`${dirs.DocumentDir}/Boostnote/test.md`, '# Welcome to Boostnote :)\nThis is a markdown note.', 'utf8')
+                        .catch(err => console.log(err));
+                }
+                return this.listFiles(`${dirs.DocumentDir}/Boostnote`);
+            })
+            .then((files) => {
+                console.log(files);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
 
     openDrawer = () => {
         this._drawer._root.open();
@@ -36,6 +71,26 @@ export default class App extends Component {
         this.setState({isNoteOpen: isOpen})
     }
 
+    listFiles(dir) {
+        return RNFetchBlob.fs.ls(dir);
+    }
+
+    createDir() {
+        RNFetchBlob.fs.mkdir(`${dirs.DocumentDir}/Boostnote`)
+            .then(() => {
+                console.log('OK');
+            })
+            .catch((err) => {
+                console.log('NG');
+            });
+    }
+
+    createNewNote() {
+        const dirs = RNFetchBlob.fs.dirs;
+        alert(`${dirs.DocumentDir}/Boostnote`);
+        fs.createFile(`${dirs.DocumentDir}/Boostnote`, '', 'utf8');
+    }
+
     render() {
         return (
             <Drawer
@@ -43,7 +98,7 @@ export default class App extends Component {
                     this._drawer = ref;
                 }}
                 content={<SideBar/>}
-                panOpenMask={.75}>
+                panOpenMask={.05}>
                 <Container>
                     <Header style={Platform.OS === 'android' ? {height: 70} : null}>
                         <Left style={Platform.OS === 'android' ? {top: 10} : null}>
@@ -61,190 +116,16 @@ export default class App extends Component {
                         </Right>
                     </Header>
                     <Content>
-                        <Card>
-                            <CardItem button onPress={() => this.setNoteModalIsOpen(true)}>
-                                <Body>
-                                <Text>aaaaaaaaaaa</Text>
-                                </Body>
-                            </CardItem>
-                        </Card>
-                        <Card>
-                            <CardItem>
-                                <Body>
-                                <Text>bbbbbbbbbbbbb</Text>
-                                </Body>
-                            </CardItem>
-                        </Card><Card>
-                        <CardItem>
-                            <Body>
-                            <Text>aaaaaaaaaaa</Text>
-                            </Body>
-                        </CardItem>
-                    </Card>
-                        <Card>
-                            <CardItem>
-                                <Body>
-                                <Text>bbbbbbbbbbbbb</Text>
-                                </Body>
-                            </CardItem>
-                        </Card><Card>
-                        <CardItem>
-                            <Body>
-                            <Text>aaaaaaaaaaa</Text>
-                            </Body>
-                        </CardItem>
-                    </Card>
-                        <Card>
-                            <CardItem>
-                                <Body>
-                                <Text>bbbbbbbbbbbbb</Text>
-                                </Body>
-                            </CardItem>
-                        </Card><Card>
-                        <CardItem>
-                            <Body>
-                            <Text>aaaaaaaaaaa</Text>
-                            </Body>
-                        </CardItem>
-                    </Card>
-                        <Card>
-                            <CardItem>
-                                <Body>
-                                <Text>bbbbbbbbbbbbb</Text>
-                                </Body>
-                            </CardItem>
-                        </Card><Card>
-                        <CardItem>
-                            <Body>
-                            <Text>aaaaaaaaaaa</Text>
-                            </Body>
-                        </CardItem>
-                    </Card>
-                        <Card>
-                            <CardItem>
-                                <Body>
-                                <Text>bbbbbbbbbbbbb</Text>
-                                </Body>
-                            </CardItem>
-                        </Card><Card>
-                        <CardItem>
-                            <Body>
-                            <Text>aaaaaaaaaaa</Text>
-                            </Body>
-                        </CardItem>
-                    </Card>
-                        <Card>
-                            <CardItem>
-                                <Body>
-                                <Text>bbbbbbbbbbbbb</Text>
-                                </Body>
-                            </CardItem>
-                        </Card><Card>
-                        <CardItem>
-                            <Body>
-                            <Text>aaaaaaaaaaa</Text>
-                            </Body>
-                        </CardItem>
-                    </Card>
-                        <Card>
-                            <CardItem>
-                                <Body>
-                                <Text>bbbbbbbbbbbbb</Text>
-                                </Body>
-                            </CardItem>
-                        </Card><Card>
-                        <CardItem>
-                            <Body>
-                            <Text>aaaaaaaaaaa</Text>
-                            </Body>
-                        </CardItem>
-                    </Card>
-                        <Card>
-                            <CardItem>
-                                <Body>
-                                <Text>bbbbbbbbbbbbb</Text>
-                                </Body>
-                            </CardItem>
-                        </Card><Card>
-                        <CardItem>
-                            <Body>
-                            <Text>aaaaaaaaaaa</Text>
-                            </Body>
-                        </CardItem>
-                    </Card>
-                        <Card>
-                            <CardItem>
-                                <Body>
-                                <Text>bbbbbbbbbbbbb</Text>
-                                </Body>
-                            </CardItem>
-                        </Card><Card>
-                        <CardItem>
-                            <Body>
-                            <Text>aaaaaaaaaaa</Text>
-                            </Body>
-                        </CardItem>
-                    </Card>
-                        <Card>
-                            <CardItem>
-                                <Body>
-                                <Text>bbbbbbbbbbbbb</Text>
-                                </Body>
-                            </CardItem>
-                        </Card><Card>
-                        <CardItem>
-                            <Body>
-                            <Text>aaaaaaaaaaa</Text>
-                            </Body>
-                        </CardItem>
-                    </Card>
-                        <Card>
-                            <CardItem>
-                                <Body>
-                                <Text>bbbbbbbbbbbbb</Text>
-                                </Body>
-                            </CardItem>
-                        </Card><Card>
-                        <CardItem>
-                            <Body>
-                            <Text>aaaaaaaaaaa</Text>
-                            </Body>
-                        </CardItem>
-                    </Card>
-                        <Card>
-                            <CardItem>
-                                <Body>
-                                <Text>bbbbbbbbbbbbb</Text>
-                                </Body>
-                            </CardItem>
-                        </Card><Card>
-                        <CardItem>
-                            <Body>
-                            <Text>aaaaaaaaaaa</Text>
-                            </Body>
-                        </CardItem>
-                    </Card>
-                        <Card>
-                            <CardItem>
-                                <Body>
-                                <Text>bbbbbbbbbbbbb</Text>
-                                </Body>
-                            </CardItem>
-                        </Card><Card>
-                        <CardItem>
-                            <Body>
-                            <Text>aaaaaaaaaaa</Text>
-                            </Body>
-                        </CardItem>
-                    </Card>
-                        <Card>
-                            <CardItem>
-                                <Body>
-                                <Text>bbbbbbbbbbbbb</Text>
-                                </Body>
-                            </CardItem>
-                        </Card>
+
                     </Content>
+                    <Fab
+                        active={true}
+                        containerStyle={{ marginLeft: 10 }}
+                        style={{ backgroundColor: '#5067FF' }}
+                        position="bottomRight"
+                        onPress={() => this.createNewNote()}>
+                        <Icon name="md-add" />
+                    </Fab>
                     <NoteModal setIsOpen={this.setNoteModalIsOpen.bind(this)} isNoteOpen={this.state.isNoteOpen}/>
                 </Container>
             </Drawer>
