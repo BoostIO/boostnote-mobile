dbc005a21f89f18e5fd41eed5051cbe7b5782819
Kohei TAKATA <kt.koheitakata@gmail.com>
2017-10-02 23:52:06 +0900

    remove semicolon

--- app/lib/CofeeScriptEval.js
+++ app/lib/CofeeScriptEval.js
@@ -1,11 +1,11 @@
-const CoffeeScript = require('coffeescript/lib/coffeescript/coffeescript');
-const compile = CoffeeScript.compile;
+const CoffeeScript = require('coffeescript/lib/coffeescript/coffeescript')
+const compile = CoffeeScript.compile
 
 CoffeeScript.eval = function(code, options = {}) {
     if (options.bare == null) {
-        options.bare = true;
+        options.bare = true
     }
-    return eval(compile(code, options));
-};
+    return eval(compile(code, options))
+}
 
-module.exports = CoffeeScript;
+module.exports = CoffeeScript
