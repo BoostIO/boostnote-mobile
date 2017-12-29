75c4f4027b7b0adb12460d575c545b3bbab8cd7b
Sosuke Suzuki <aosukeke@gmail.com>
2017-12-29 17:22:23 +0900

    add styles

--- app/views/note/DropboxNoteModal.js
+++ app/views/note/DropboxNoteModal.js
@@ -37,19 +37,35 @@ const DROPBOX_ACCESS_TOKEN = 'DROPBOX:ACCESS_TOKEN'
 
 const styles = {
   switchButton: {
-    backgroundColor: 'transparent',
-    borderColor: '#EFF1F5',
-    borderWidth: 1
+      backgroundColor: 'transparent',
+      borderColor: '#1ec38b',
+      borderWidth: 1
   },
   switchButtonActive: {
-    backgroundColor: '#EFF1F5',
-    borderColor: '#EFF1F5',
-    borderWidth: 1
+      backgroundColor: '#EFF1F5',
+      borderColor: '#1ec38b',
+      borderWidth: 1
+  },
+  switchEditButton: {
+    position:'absolute',
+    width: 40,
+    right: 20
+  },
+  switchEditText: {
+    color: '#1ec38b',
+    fontSize: 16,
+    lineHeight: 16
   },
   noteDetailButton: {
-    color: '#EFF1F5',
-    fontSize: 23
+      color: '#1ec38b',
+      fontSize: 23
   },
+  backHomeText: {
+      color: '#1ec38b',
+      fontSize: 17,
+      lineHeight: 17,
+      paddingLeft: 10
+  }
 }
 
 export default class DropboxNoteModal extends React.Component {
